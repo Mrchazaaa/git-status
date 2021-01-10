70d1d8cff40d2e1c677990d966a79d9e5426e611
charlie <charlie.howlett@live.co.uk>
2021-01-10 22:28:37 +0000

    test commit

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ app.listen(port, () => {
 app.post('/handle',(request,response) => {
     //code to perform particular action.
     //To access POST variable use req.body()methods.
-    // console.log(request);
     console.log(request.body);
     response.status(200).end() // Responding is important
 });
