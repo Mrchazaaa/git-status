00b73c43fb6cf4dcaad84efc28a55267bb85e52f
charlie <charlie.howlett@live.co.uk>
2021-01-10 22:27:22 +0000

    Added body-parser

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
 
 app.use(express.static('../client/build'));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
@@ -11,6 +13,7 @@ app.listen(port, () => {
 app.post('/handle',(request,response) => {
     //code to perform particular action.
     //To access POST variable use req.body()methods.
-    console.log(request);
+    // console.log(request);
     console.log(request.body);
+    response.status(200).end() // Responding is important
 });
